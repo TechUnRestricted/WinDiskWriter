02a3d7656d4d78767499aedcbefe0b2f98c98b4b
TechUnRestricted <83237609+TechUnRestricted@users.noreply.github.com>
2023-01-27 16:06:43 +0300

    EA6E6F47-B6B5-4D35-8F72-12DFE78096DF

--- windiskwriter/DAWrapper.m
+++ windiskwriter/DAWrapper.m
@@ -46,4 +46,39 @@
     return self;
 }
 
+- (struct DiskInfo)getDiskInfo {
+    struct DiskInfo diskInfo;
+    NSDictionary *diskDescription = CFBridgingRelease(DADiskCopyDescription(currentDisk));
+    
+    diskInfo.isDrive = [[diskDescription objectForKey:@"DAMediaWhole"] boolValue];
+    diskInfo.isInternal = [[diskDescription objectForKey:@"DADeviceInternal"] boolValue];
+    diskInfo.isMountable = [[diskDescription objectForKey:@"DAVolumeMountable"] boolValue];
+    diskInfo.isRemovable = [[diskDescription objectForKey:@"DAMediaRemovable"] boolValue];
+    diskInfo.isDeviceUnit = [[diskDescription objectForKey:@"DADeviceUnit"] boolValue];
+    diskInfo.isWritable = [[diskDescription objectForKey:@"DAMediaWritable"] boolValue];
+    diskInfo.isBSDUnit = [[diskDescription objectForKey:@"DAMediaBSDUnit"] boolValue];
+    diskInfo.isEncrypted = [[diskDescription objectForKey:@"DAMediaEncrypted"] boolValue];
+    diskInfo.isNetworkVolume = [[diskDescription objectForKey:@"DAVolumeNetwork"] boolValue];
+    diskInfo.isEjectable = [[diskDescription objectForKey:@"DAMediaEjectable"] boolValue];
+
+    diskInfo.mediaSize = [diskDescription objectForKey:@"DAMediaSize"];
+    diskInfo.blockSize = [diskDescription objectForKey:@"DAMediaBlockSize"];
+    diskInfo.appearanceTime = [diskDescription objectForKey:@"DAAppearanceTime"];
+
+    diskInfo.devicePath = [diskDescription objectForKey:@"DADevicePath"];
+    diskInfo.deviceModel = [diskDescription objectForKey:@"DADeviceModel"];
+    diskInfo.BSDName = [diskDescription objectForKey:@"DAMediaBSDName"];
+    diskInfo.mediaKind = [diskDescription objectForKey:@"DAMediaKind"];
+    diskInfo.mediaPath = [diskDescription objectForKey:@"DAMediaPath"];
+    diskInfo.mediaName = [diskDescription objectForKey:@"DAMediaName"];
+    diskInfo.mediaContent = [diskDescription objectForKey:@"DAMediaContent"];
+    diskInfo.busPath = [diskDescription objectForKey:@"DABusPath"];
+    diskInfo.deviceProtocol = [diskDescription objectForKey:@"DADeviceProtocol"];
+    diskInfo.deviceRevision = [diskDescription objectForKey:@"DADeviceRevision"];
+    diskInfo.busName = [diskDescription objectForKey:@"DABusName"];
+    diskInfo.deviceVendor = [diskDescription objectForKey:@"DADeviceVendor"];
+    
+    return diskInfo;
+}
+
 @end
