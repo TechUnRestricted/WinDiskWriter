c05e0c91d96273271b6f8de736c7e5e2c5aee64c
TechUnRestricted <83237609+TechUnRestricted@users.noreply.github.com>
2023-01-27 16:06:43 +0300

    EA6E6F47-B6B5-4D35-8F72-12DFE78096DF

--- windiskwriter/DAWrapper.h
+++ windiskwriter/DAWrapper.h
@@ -10,10 +10,42 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+struct DiskInfo {
+    BOOL isDrive;
+    BOOL isInternal;
+    BOOL isMountable;
+    BOOL isRemovable;
+    BOOL isDeviceUnit;
+    BOOL isWritable;
+    BOOL isBSDUnit;
+    BOOL isEncrypted;
+    BOOL isNetworkVolume;
+    BOOL isEjectable;
+    
+    NSNumber *mediaSize;
+    NSNumber *blockSize;
+    NSNumber *appearanceTime;
+    
+    NSString *devicePath;
+    NSString *deviceModel;
+    NSString *BSDName;
+    NSString *mediaKind;
+    NSString *mediaPath;
+    NSString *mediaName;
+    NSString *mediaContent;
+    NSString *busPath;
+    NSString *deviceProtocol;
+    NSString *deviceRevision;
+    NSString *busName;
+    NSString *deviceVendor;
+};
+
 @interface DAWrapper : NSObject
 - (instancetype)init NS_UNAVAILABLE;
 - (instancetype _Nullable)initWithBSDName: (NSString * _Nonnull)bsdName;
 - (instancetype _Nullable)initWithVolumePath: (NSString * _Nonnull)volumePath;
+
+- (struct DiskInfo)getDiskInfo;
 @end
 
 NS_ASSUME_NONNULL_END
