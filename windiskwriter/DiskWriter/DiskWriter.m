0da3d706b5f14029d8a6bcaa4c07b7a786623db7
TechUnRestricted <83237609+TechUnRestricted@users.noreply.github.com>
2023-02-04 01:24:33 +0300

    1953F085-1CC0-4531-AF23-715D1F8D89DF

--- windiskwriter/DiskWriter/DiskWriter.m
+++ windiskwriter/DiskWriter/DiskWriter.m
@@ -14,6 +14,7 @@
 #import "Filesystems.h"
 #import "HelperFunctions.h"
 #import "NSString+Common.h"
+#import "wimlib.h"
 
 const uint32_t FAT32_MAX_FILE_SIZE = 4294967295;
 
@@ -79,10 +80,6 @@ const uint32_t FAT32_MAX_FILE_SIZE = 4294967295;
         }
     }
     
-    if (destinationDeviceDM == NULL) {
-        
-    }
-    
     if ([destinationDeviceDM getDiskInfo].BSDName == NULL) {
         DebugLog(@"The specified destination device is invalid.");
     }
@@ -90,14 +87,140 @@ const uint32_t FAT32_MAX_FILE_SIZE = 4294967295;
     return destinationDeviceDM;
 }
 
-+ (BOOL)splitWIMWithOriginFilePath: (NSString *)originWIMFilePath
-            destinationWIMFilePath: (NSString *)destinationWIMFilePath {
+static enum wimlib_progress_status extractProgress(enum wimlib_progress_msg msg,
+                                                   union wimlib_progress_info *info,
+                                                   void *progctx) {
+    /*
+     WIMLIB_PROGRESS_MSG_SPLIT_BEGIN_PART
+     WIMLIB_PROGRESS_MSG_WRITE_STREAMS
+     
+     WIMLIB_PROGRESS_MSG_SPLIT_END_PART
+     WIMLIB_PROGRESS_MSG_SPLIT_BEGIN_PART
+     WIMLIB_PROGRESS_MSG_WRITE_STREAMS
+     
+     WIMLIB_PROGRESS_MSG_WRITE_METADATA_BEGIN
+     WIMLIB_PROGRESS_MSG_WRITE_METADATA_END
+     
+     WIMLIB_PROGRESS_MSG_SPLIT_END_PART
+     */
+
+    switch (msg) {
+            
+        case WIMLIB_PROGRESS_MSG_EXTRACT_IMAGE_BEGIN:
+            NSLog(@"WIMLIB_PROGRESS_MSG_EXTRACT_IMAGE_BEGIN");
+            break;
+        case WIMLIB_PROGRESS_MSG_EXTRACT_TREE_BEGIN:
+            NSLog(@"WIMLIB_PROGRESS_MSG_EXTRACT_TREE_BEGIN");
+            break;
+        case WIMLIB_PROGRESS_MSG_EXTRACT_FILE_STRUCTURE:
+            NSLog(@"WIMLIB_PROGRESS_MSG_EXTRACT_FILE_STRUCTURE");
+            break;
+        case WIMLIB_PROGRESS_MSG_EXTRACT_STREAMS:
+            NSLog(@"WIMLIB_PROGRESS_MSG_EXTRACT_STREAMS");
+            break;
+        case WIMLIB_PROGRESS_MSG_EXTRACT_SPWM_PART_BEGIN:
+            NSLog(@"WIMLIB_PROGRESS_MSG_EXTRACT_SPWM_PART_BEGIN");
+            break;
+        case WIMLIB_PROGRESS_MSG_EXTRACT_METADATA:
+            NSLog(@"WIMLIB_PROGRESS_MSG_EXTRACT_METADATA");
+            break;
+        case WIMLIB_PROGRESS_MSG_EXTRACT_IMAGE_END:
+            NSLog(@"WIMLIB_PROGRESS_MSG_EXTRACT_IMAGE_END");
+            break;
+        case WIMLIB_PROGRESS_MSG_EXTRACT_TREE_END:
+            NSLog(@"WIMLIB_PROGRESS_MSG_EXTRACT_TREE_END");
+            break;
+        case WIMLIB_PROGRESS_MSG_SCAN_BEGIN:
+            NSLog(@"WIMLIB_PROGRESS_MSG_SCAN_BEGIN");
+            break;
+        case WIMLIB_PROGRESS_MSG_SCAN_DENTRY:
+            NSLog(@"WIMLIB_PROGRESS_MSG_SCAN_DENTRY");
+            break;
+        case WIMLIB_PROGRESS_MSG_SCAN_END:
+            NSLog(@"WIMLIB_PROGRESS_MSG_SCAN_END");
+            break;
+        case WIMLIB_PROGRESS_MSG_WRITE_STREAMS:
+            NSLog(@"WIMLIB_PROGRESS_MSG_WRITE_STREAMS");
+            break;
+        case WIMLIB_PROGRESS_MSG_WRITE_METADATA_BEGIN:
+            NSLog(@"WIMLIB_PROGRESS_MSG_WRITE_METADATA_BEGIN");
+            break;
+        case WIMLIB_PROGRESS_MSG_WRITE_METADATA_END:
+            NSLog(@"WIMLIB_PROGRESS_MSG_WRITE_METADATA_END");
+            break;
+        case WIMLIB_PROGRESS_MSG_RENAME:
+            NSLog(@"WIMLIB_PROGRESS_MSG_RENAME");
+            break;
+        case WIMLIB_PROGRESS_MSG_VERIFY_INTEGRITY:
+            NSLog(@"WIMLIB_PROGRESS_MSG_VERIFY_INTEGRITY");
+            break;
+        case WIMLIB_PROGRESS_MSG_CALC_INTEGRITY:
+            NSLog(@"WIMLIB_PROGRESS_MSG_CALC_INTEGRITY");
+            break;
+        case WIMLIB_PROGRESS_MSG_SPLIT_BEGIN_PART:
+            NSLog(@"WIMLIB_PROGRESS_MSG_SPLIT_BEGIN_PART");
+            break;
+        case WIMLIB_PROGRESS_MSG_SPLIT_END_PART:
+            NSLog(@"WIMLIB_PROGRESS_MSG_SPLIT_END_PART");
+            break;
+        case WIMLIB_PROGRESS_MSG_UPDATE_BEGIN_COMMAND:
+            NSLog(@"WIMLIB_PROGRESS_MSG_UPDATE_BEGIN_COMMAND");
+            break;
+        case WIMLIB_PROGRESS_MSG_UPDATE_END_COMMAND:
+            NSLog(@"WIMLIB_PROGRESS_MSG_UPDATE_END_COMMAND");
+            break;
+        case WIMLIB_PROGRESS_MSG_REPLACE_FILE_IN_WIM:
+            NSLog(@"WIMLIB_PROGRESS_MSG_REPLACE_FILE_IN_WIM");
+            break;
+        case WIMLIB_PROGRESS_MSG_WIMBOOT_EXCLUDE:
+            NSLog(@"WIMLIB_PROGRESS_MSG_WIMBOOT_EXCLUDE");
+            break;
+        case WIMLIB_PROGRESS_MSG_UNMOUNT_BEGIN:
+            NSLog(@"WIMLIB_PROGRESS_MSG_UNMOUNT_BEGIN");
+            break;
+        case WIMLIB_PROGRESS_MSG_DONE_WITH_FILE:
+            NSLog(@"WIMLIB_PROGRESS_MSG_DONE_WITH_FILE");
+            break;
+        case WIMLIB_PROGRESS_MSG_BEGIN_VERIFY_IMAGE:
+            NSLog(@"WIMLIB_PROGRESS_MSG_BEGIN_VERIFY_IMAGE");
+            break;
+        case WIMLIB_PROGRESS_MSG_END_VERIFY_IMAGE:
+            NSLog(@"WIMLIB_PROGRESS_MSG_END_VERIFY_IMAGE");
+            break;
+        case WIMLIB_PROGRESS_MSG_VERIFY_STREAMS:
+            NSLog(@"WIMLIB_PROGRESS_MSG_VERIFY_STREAMS");
+            break;
+        case WIMLIB_PROGRESS_MSG_TEST_FILE_EXCLUSION:
+            NSLog(@"WIMLIB_PROGRESS_MSG_TEST_FILE_EXCLUSION");
+            break;
+        case WIMLIB_PROGRESS_MSG_HANDLE_ERROR:
+            NSLog(@"WIMLIB_PROGRESS_MSG_HANDLE_ERROR");
+            break;
+    }
+
+    return WIMLIB_PROGRESS_STATUS_CONTINUE;
+}
+
++ (enum wimlib_error_code)splitWIMWithOriginFilePath: (NSString *)originWIMFilePath
+            destinationWIMFilePath: (NSString *)destinationWIMFilePath
+               maxSliceSizeInBytes: (uint64_t *)maxSliceSizeInBytes{
+    WIMStruct *currentWIM;
     
+    enum wimlib_error_code wimOpenReturn = wimlib_open_wim([originWIMFilePath UTF8String], 0, &currentWIM);
+    wimlib_register_progress_function(currentWIM, extractProgress, NULL);
+   
+    NSString *destinationFileName = [[[originWIMFilePath lastPathComponent] stringByDeletingPathExtension] stringByAppendingPathExtension:@"swm"];
+    
+    enum wimlib_error_code splitResultReturn = wimlib_split(currentWIM, [destinationFileName UTF8String], maxSliceSizeInBytes, NULL);
+    
+    wimlib_free(currentWIM);
+    
+    return splitResultReturn;
 }
 
 + (BOOL)writeWindows11ISOWithSourcePath: (NSString *)sourcePath
                         destinationPath: (NSString *)destinationPath
-                  bypassTPMRequirements: (BOOL)bypassTPMRequirements
+                  bypassTPMRequirements: (BOOL)bypassTPMAndSecureBootRequirements
                                 isFAT32: (BOOL)isFAT32 { // TODO: Come up with a more elegant solution
     
     NSFileManager *localFileManager = [NSFileManager defaultManager];
@@ -133,14 +256,26 @@ const uint32_t FAT32_MAX_FILE_SIZE = 4294967295;
             continue;
         }
         
-        if (isFAT32 && [currentFileAttributes fileSize] > FAT32_MAX_FILE_SIZE /* destinationDiskInfo.mediaKind */) {
+        /* WIM Handler */
+        if (isFAT32 && [currentFileAttributes fileSize] > FAT32_MAX_FILE_SIZE) {
             NSString *filePathExtension = [[sourceFilePath lowercaseString] pathExtension];
             
             if ([filePathExtension isEqualToString: @"wim"]) {
                 // TODO: Implement .wim file splitting
-                BOOL wimSplitSuccessful = [DiskWriter splitWIMWithOriginFilePath:sourceFilePath
-                                                          destinationWIMFilePath:[destinationPasteFilePath stringByDeletingLastPathComponent]];
-                // DebugLog(<#...#>)
+                enum wimlib_error_code wimSplitResult = [DiskWriter splitWIMWithOriginFilePath: sourceFilePath
+                                                                        destinationWIMFilePath: [destinationPasteFilePath stringByDeletingLastPathComponent] maxSliceSizeInBytes: 1500000000
+                ];
+                
+                
+                
+                DebugLog(@"Splitting the .wim image for several parts was %@.",
+                         (wimSplitResult == WIMLIB_ERR_SUCCESS ? @"successful" : @"unsuccesful")
+                );
+                
+                if (wimSplitResult != WIMLIB_ERR_SUCCESS) {
+                    return NO;
+                }
+                
             } else if ([filePathExtension isEqualToString:@"esd"]) {
                 // TODO: Implement .esd file splitting
                 DebugLog(@"Splitting .esd files into multiple parts is currently not available. Further copying is pointless...");
