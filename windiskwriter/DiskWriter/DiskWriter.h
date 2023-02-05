955b0a1d5e35a0373a264a88ad5723896861bfb1
TechUnRestricted <83237609+TechUnRestricted@users.noreply.github.com>
2023-02-05 16:10:36 +0300

    C96935AF-6101-4CF2-83BA-7C61A7077500

--- windiskwriter/DiskWriter/DiskWriter.h
+++ windiskwriter/DiskWriter/DiskWriter.h
@@ -9,16 +9,36 @@
 #import "BootModes.h"
 
 NS_ASSUME_NONNULL_BEGIN
+
+enum FileWriteResult {
+    FileWriteResultFailure,
+    FileWriteResultSuccess,
+    FileWriteResultCantGetFileAttributes,
+    FileWriteResultFileIsTooLarge,
+    FileWriteResultUnsupportedOperation,
+};
+
+struct FileWriteInfo {
+    NSString * _Nonnull sourceFilePath;
+    NSString * _Nonnull destinationFilePath;
+    uint64_t entitiesRemain;
+    enum FileWriteResult result;
+};
+
+typedef BOOL (^FileWriteResult)(struct FileWriteInfo);
+
 @interface DiskWriter: NSObject
 
 - (BOOL)writeWindowsISO;
 
 - (instancetype)init NS_UNAVAILABLE;
-+ (BOOL)writeWindowsISOWithSourcePath: (NSString * _Nonnull)sourcePath
-                      destinationPath: (NSString * _Nonnull)destinationPath
-   bypassTPMAndSecureBootRequirements: (BOOL)bypassTPMAndSecureBootRequirements
-                             bootMode: (BootMode)bootMode
-                              isFAT32: (BOOL)isFAT32;
++ (BOOL)writeWindows11ISOWithSourcePath: (NSString * _Nonnull)sourcePath
+                        destinationPath: (NSString * _Nonnull)destinationPath
+     bypassTPMAndSecureBootRequirements: (BOOL)bypassTPMAndSecureBootRequirements
+                               bootMode: (BootMode _Nonnull)bootMode
+                                isFAT32: (BOOL)isFAT32
+                                  error: (NSError **)error
+                               callback: (FileWriteResult _Nullable)progressTracker;
 
 @end
 
