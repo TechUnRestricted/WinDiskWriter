bcc3ed7e05cf6a1cfc710c60802558bf0fc0b664
TechUnRestricted <83237609+TechUnRestricted@users.noreply.github.com>
2023-02-09 23:28:28 +0300

    99182701-38B2-4747-957C-9EF7EF504579

--- windiskwriter/main.m
+++ windiskwriter/main.m
@@ -13,6 +13,7 @@
 #import "CommandLine.h"
 #import "DiskManager.h"
 #import "DiskWriter.h"
+#import "Constants.h"
 #import "HDIUtil.h"
 
 void printUsage(void);
@@ -26,6 +27,7 @@ enum AvailableArguments {
 
 int main(int argc, const char *argv[]) {
 	@autoreleasepool {
+		/*
 		NSError *writeError = NULL;
 		BOOL result = [DiskWriter writeWindows11ISOWithSourcePath: @"/Volumes/CCCOMA_X64FRE_RU-RU_DV9"
 												  destinationPath: @"/Volumes/D0R1K"
@@ -88,6 +90,7 @@ int main(int argc, const char *argv[]) {
 		if (writeError != NULL) {
 			IOLog(@"An error has occurred while writing the image: [%@]", [writeError userInfo]);
 		}
+		*/
 		
 		NSArray *applicationArguments = NSProcessInfo.processInfo.arguments;
 		
@@ -115,18 +118,60 @@ int main(int argc, const char *argv[]) {
 															   ]
 		];
 		
+		__block NSString *imageSource = NULL;
+		__block NSString *destinationDevice = NULL;
+		__block Filesystem filesystem = NULL;
+		__block BOOL doNotErase = NO;
+		
 		NSError *argumentsHandlerError = NULL;
 		BOOL argumentsHandlerResult = [argumentsHandler loopThroughArgumentsWithErrorHandler: &argumentsHandlerError
 																					callback: ^void(ArgumentObject * _Nonnull argumentObject, NSString * _Nullable pair) {
-			
 			switch (argumentObject.uniqueID) {
-				case ArgumentSourceDirectory:
+				case ArgumentSourceDirectory: {
+					NSError *error = NULL;
+					imageSource = [HelperFunctions getWindowsSourceMountPath:pair error:&error];
+					
+					if (error != NULL) {
+						IOLog(@"[ERROR (Can't get Image Source)]: '%@'", [[error userInfo] objectForKey:DEFAULT_ERROR_KEY]);
+						exit(EXIT_FAILURE);
+					}
 					break;
-				case ArgumentDestinationDevice:
+				}
+				case ArgumentDestinationDevice: {
+					NSError *error = NULL;
+					DiskManager *diskManager = [HelperFunctions getDiskManagerWithDevicePath:pair error:&error];
+					
+					if (error != NULL) {
+						IOLog(@"[ERROR (Can't get Destination Device)]: '%@'", [[error userInfo] objectForKey:DEFAULT_ERROR_KEY]);
+						exit(EXIT_FAILURE);
+					}
+					
+					NSString *bsdName = [diskManager getDiskInfo].BSDName;
+					if (bsdName == NULL) {
+						IOLog(@"[ERROR (Can't get Destination Device BSD Name)].");
+						exit(EXIT_FAILURE);
+					}
+					
+					destinationDevice = bsdName;
+					
 					break;
-				case ArgumentFilesystem:
+				}
+				case ArgumentFilesystem: {
+					NSString *uppercasePair = [pair uppercaseString];
+					
+					if ([uppercasePair isEqualToString:FilesystemFAT32]) {
+						filesystem = FilesystemFAT32;
+					} else if ([uppercasePair isEqualToString:FilesystemExFAT]) {
+						filesystem = FilesystemExFAT;
+					} else {
+						IOLog(@"[ERROR (Passed an unrecognized filesystem personality)]: '%@'", pair);
+						exit(EXIT_FAILURE);
+					}
+					
 					break;
+				}
 				case ArgumentDoNotErase:
+					doNotErase = YES;
 					break;
 				default:
 					break;
@@ -145,10 +190,11 @@ int main(int argc, const char *argv[]) {
 		if (argumentsHandlerError != NULL) {
 			IOLog(@"[ERROR:] %@", [[argumentsHandlerError userInfo] objectForKey:@"Reason"]);
 		}
+	
 		
 		// printUsage();
 	}
-	return 0;
+	return EXIT_SUCCESS;
 }
 
 void printUsage(void) {
